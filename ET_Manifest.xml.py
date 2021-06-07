89a7afeb2ff1dd5f50be45f1b10a26895734c7c3
Robin-lubin-2021 <asdfzxcv7571@163.com>
2021-06-07 23:40:07 +0800

    cancel print(),add notes

--- ET_Manifest.xml.py
+++ ET_Manifest.xml.py
@@ -14,7 +14,5 @@ root = tree.getroot()   # 读取树结构的根节点
 for item in root.iter("item"):  # 遍历manifest.xml文件中根节点下的所有"item"元素
     ItemDir = item.attrib  # 将"item"元素的属性赋值给ItemDir变量
     FileSourcePath = os.path.join(FileRoot, "items", ItemDir["type"], ItemDir["locator"]) # 组合成源文件路径
-    FileDestPath = os.path.dirname(os.path.join(DestPathRoot, DestPathDir[ItemDir["type"]], "LUBIN", ItemDir["locator"]))
-    print(FileSourcePath)
-    print(FileDestPath)
-    shutil.move(FileSourcePath, FileDestPath)
+    FileDestPath = os.path.dirname(os.path.join(DestPathRoot, DestPathDir[ItemDir["type"]], "LUBIN", ItemDir["locator"])) #组合成目标路径
+    shutil.move(FileSourcePath, FileDestPath)   #将源文件移动至目标路径
